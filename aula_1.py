1d4999e2f71dcc579ca6145ffe29bdd287518922
Mario Sergio <mariopekenossalro@gmail.com>
2019-06-04 21:46:13 -0300

    novas estruturas de dados incluidas no arquivo

--- aula_1.py
+++ aula_1.py
@@ -1,4 +1,47 @@
-print("Mário Sérgio é um cara muito legal!")
+# print("Mário Sérgio é um cara muito legal!")
 
+# String
 nome = "Maria"
-print(nome[0])
+# print(nome[0])
+
+# Number
+numero_inteiro = 10
+numero_real = 20.87
+
+# Boolean
+verdadeiro = True
+falso = False
+
+# Null
+nulo = None
+
+# List
+lista = [1,2,3,4,5,6,7,8,9,10]
+lista2 = ['a','b','c'] 
+lista3 = ['a', None, True, 100,[1,2,3,["Mário Sérgio", "Oliveira"]]]
+
+# Dict
+dicionario = {'nome': 'Mário',
+			  'idade': 26,
+			  'email': 'mario@email.com',
+			  'lista_certificados': ['medicina', 'auxiliar administrativo']}
+
+lista_dict = [
+		{'nome': 'Mário', 'idade': 26, 'email': 'mario@email.com'},
+		{'nome': 'João', 'idade': 26, 'email': 'mario@email.com'},
+		{'nome': 'Rose', 'idade': 26, 'email': 'mario@email.com'},
+		{'nome': 'Jessica', 'idade': 26, 'email': 'mario@email.com'}
+]
+
+lista_dict_2 = {
+		'pessoa_1': {'nome': 'Mário', 'idade': 26, 'email': 'mario@email.com'},
+		1: {'nome': 'João', 'idade': 26, 'email': 'mario@email.com'},
+		'pessoa_3': {'nome': 'Rose', 'idade': 26, 'email': 'mario@email.com'},
+		3: {'nome': 'Jessica', 'idade': 26, 'email': 'mario@email.com'}
+}
+
+# print(lista_dict[2]['nome'])
+
+# Tuple
+tupla = (1,2)
+tupla = (1,2,'sdfjsd',1110)
