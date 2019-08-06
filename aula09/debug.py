8de570648273185cc6a4fcbd9842c542c5e4ba1f
Mario Sergio <mariopekenossalro@gmail.com>
2019-08-06 19:32:25 -0300

    aula 09

---
+++ aula09/debug.py
@@ -0,0 +1,19 @@
+numero1 = 10
+numero2 = 20
+resultado = None
+breakpoint()
+
+for num in range(10):
+	print(num**2)
+
+soma = numero1 + numero2
+
+mult = soma*8
+
+soma = 0
+
+mult = -1
+
+resultado = soma + numero1
+
+print(resultado)
