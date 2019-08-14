b957a97cd06f8f73e90088a2e6d4510c1ed37fa4
Mario Sergio <mariopekenossalro@gmail.com>
2019-08-14 11:08:39 -0300

    aula 11

--- aula10/forum/polls/admin.py
+++ aula10/forum/polls/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Question, Choice
+from .models import Question, Choice, Person
 
 admin.site.register(Question)
 admin.site.register(Choice)
+admin.site.register(Person)
--- aula10/forum/polls/models.py
+++ aula10/forum/polls/models.py
@@ -1,18 +1,28 @@
 from django.db import models
 
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+class Person(models.Model):
+    name = models.CharField(max_length=300)
+    email = models.EmailField()
 
     def __str__(self):
-    	return self.question_text
+        return self.name
+
+class Question(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.question_text
 
 
 class Choice(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-    	return self.choice_text
+        return self.choice_text
