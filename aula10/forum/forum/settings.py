a02022d1775db5c5cb2991e88341754575cf3431
Mario Sergio <mariopekenossalro@gmail.com>
2019-08-20 21:23:31 -0300

    aula 12

--- aula10/forum/forum/settings.py
+++ aula10/forum/forum/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'forum.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['polls'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- aula10/forum/polls/views.py
+++ aula10/forum/polls/views.py
@@ -1,5 +1,16 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from polls.models import Question, Choice
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def questions(request):
+    questions = list(Question.objects.all().values('id', 'question_text'))
+
+    for question in questions:
+        question['choices'] = list(Choice.objects.filter(question=question['id']).values('choice_text'))
+    
+    return render(request, 'template/questions.html', {'questions': questions})
